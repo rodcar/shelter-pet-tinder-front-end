804dc548da7193c4cf7045f0ebfaef873f850a6f
Alejandra <mae880629@hotmail.com>
2022-08-12 21:40:13 -0500

    Add footer

--- js/main.js
+++ js/main.js
@@ -28,3 +28,12 @@ function linkAction() {
 	navMenu.classList.remove("show-menu");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+	const header = document.getElementById("header");
+	// When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+	if (this.scrollY >= 80) header.classList.add("scroll-header");
+	else header.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
